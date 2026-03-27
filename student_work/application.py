f10e0721dbfb2c4fa1cfbf816f80082c722f6fd0
TennaFanCodesNonsense <s1010110@student.knoxschools.org>
2026-03-27 14:36:00 +0000

    Base Skeleton Code Structure

--- student_work/application.py
+++ student_work/application.py
@@ -1,3 +1,24 @@
-# Your code will go here
+from flask import Flask, request, redirect, url_for, render_template_string, session
 
-print("cheeselord")
+# Password Dict
+users = {
+    "placeholder" : "coolpassword"
+}
+
+# HTML Code
+
+<style> </style>
+
+# Login Screen Functions
+
+def login():
+    pass
+
+def register():
+    pass
+
+def webpage():
+    pass
+
+def logout():
+    pass
